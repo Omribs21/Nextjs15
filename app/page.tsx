ee151903c9c5b767f5b5542cd9b8176bfaa82c3a
omri <omr2361@gmail.com>
2024-11-14 20:24:13 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -64,6 +65,7 @@ export default function Home() {
             height={16}
           />
           Learn
+          <Button>click me!</Button>
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
